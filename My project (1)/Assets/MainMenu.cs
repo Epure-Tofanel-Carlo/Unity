e6f2c90228af953c1592a20244acc2f2cfa6659c
AlexandraMarinaBerlinschi <alexandra-marina.berlinschi@s.unibuc.ro>
2024-04-24 08:14:52 +0300

    mainMenu + optionsMenu

--- My project (1)/Assets/MainMenu.cs
+++ My project (1)/Assets/MainMenu.cs
@@ -7,12 +7,12 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1); // daca aleg play, ma baga in scena 1 care este de fapt jocul
     }
 
-    public void Quit()
+    public void Quit() 
     {
-        Debug.Log("QUIT");
-        Application.Quit();
+        Debug.Log("QUIT"); // pun un log ca sa vad daca functioneaza butonul
+        Application.Quit(); 
     }
 }
