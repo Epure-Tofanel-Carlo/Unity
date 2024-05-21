916a8c43a958f7e8db7b5f8063629771d64fec6c
AlexandraMarinaBerlinschi <alexandra-marina.berlinschi@s.unibuc.ro>
2024-05-22 01:26:26 +0300

    scene

---
+++ My project (1)/Assets/Scripts/GameControl.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    public static event Action HandlePulled = delegate { };
+
+    [SerializeField]
+    private TextMeshProUGUI prizeText; 
+
+    [SerializeField]
+    private Row[] rows;
+
+    [SerializeField]
+    private Transform handle;
+
+    private int prizeValue;
+
+    private bool resultsChecked = false;
+
+    void Update()
+    {
+        // Daca un row se invarte nu actionez cu nimic
+        if (!rows[0].rowStopped || !rows[1].rowStopped || !rows[2].rowStopped)
+        {
+            prizeValue = 0;
+            prizeText.enabled = false;
+            resultsChecked = false;
+        }
+        // Daca toate sloturile s-au oprit, pot incepe sa calculez prize-ul
+        else if (rows[0].rowStopped && rows[1].rowStopped && rows[2].rowStopped && !resultsChecked)
+        {
+            CheckResults();
+            prizeText.enabled = true;
+            prizeText.text = "Prize: " + prizeValue;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (rows[0].rowStopped && rows[1].rowStopped && rows[2].rowStopped)
+            StartCoroutine(PullHandle());
+    }
+
+    private IEnumerator PullHandle()
+    {
+        // Roteste handle-ul la aproximativ 15 grade cand este apasat 
+        for (int i = 0; i < 15; i += 5)
+        {
+            handle.Rotate(0f, 0f, i);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        HandlePulled();
+
+        for (int i = 0; i < 15; i += 5)
+        {
+            handle.Rotate(0f, 0f, -i);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void CheckResults()
+    {
+        // Optiuni pentru punctaj
+        if (rows[0].stoppedSlot == "Diamond" && rows[1].stoppedSlot == "Diamond" && rows[2].stoppedSlot == "Diamond")
+            prizeValue = 200;
+        else if (rows[0].stoppedSlot == "Crown" && rows[1].stoppedSlot == "Crown" && rows[2].stoppedSlot == "Crown")
+            prizeValue = 400;
+        else if (rows[0].stoppedSlot == "Melon" && rows[1].stoppedSlot == "Melon" && rows[2].stoppedSlot == "Melon")
+            prizeValue = 600;
+        else if (rows[0].stoppedSlot == "Bar" && rows[1].stoppedSlot == "Bar" && rows[2].stoppedSlot == "Bar")
+            prizeValue = 800;
+        else if (rows[0].stoppedSlot == "Seven" && rows[1].stoppedSlot == "Seven" && rows[2].stoppedSlot == "Seven")
+            prizeValue = 1500;
+        else if (rows[0].stoppedSlot == "Cherry" && rows[1].stoppedSlot == "Cherry" && rows[2].stoppedSlot == "Cherry")
+            prizeValue = 3000;
+        else if (rows[0].stoppedSlot == "Lemon" && rows[1].stoppedSlot == "Lemon" && rows[2].stoppedSlot == "Lemon")
+            prizeValue = 5000;
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Diamond") ||
+                 (rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Diamond") ||
+                 (rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Diamond"))
+            prizeValue = 100;
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Crown") ||
+                 (rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Crown") ||
+                 (rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Crown"))
+            prizeValue = 300;
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Melon") ||
+                 (rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Melon") ||
+                 (rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Melon"))
+            prizeValue = 500;
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Bar") ||
+                 (rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Bar") ||
+                 (rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Bar"))
+            prizeValue = 700;
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Seven") ||
+                 (rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Seven") ||
+                 (rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Seven"))
+            prizeValue = 1000;
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Cherry") ||
+                 (rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Cherry") ||
+                 (rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Cherry"))
+            prizeValue = 2000;
+        else if ((rows[0].stoppedSlot == rows[1].stoppedSlot && rows[0].stoppedSlot == "Lemon") ||
+                 (rows[0].stoppedSlot == rows[2].stoppedSlot && rows[0].stoppedSlot == "Lemon") ||
+                 (rows[1].stoppedSlot == rows[2].stoppedSlot && rows[1].stoppedSlot == "Lemon"))
+            prizeValue = 4000;
+
+        resultsChecked = true;
+    }
+}
+
+// Definirea clasei ParticleSystemAnimationRowMode pentru a avea proprietatea rowStopped
+public class ParticleSystemAnimationRowMode
+{
+    public bool rowStopped;
+    public string stoppedSlot;
+}
---
+++ My project (1)/Assets/Scripts/Row.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Row : MonoBehaviour
+{
+    private int randomValue;
+    private float timeInterval;
+
+    public bool rowStopped; 
+    public string stoppedSlot; 
+
+    void Start()
+    {
+        rowStopped = true; // Initial, randul este oprit
+        GameControl.HandlePulled += StartRotating; 
+    }
+
+    private void StartRotating()
+    {
+        stoppedSlot = ""; // Reseteaza simbolul oprit
+        StartCoroutine("Rotate"); 
+    }
+
+    // Functia de rotatie, calculeaza pozitia folosind grade
+    private IEnumerator Rotate()
+    {
+        rowStopped = false; // Seteaza flag-ul pentru a indica ca randul se roteste
+        timeInterval = 0.025f; 
+
+        // Roteste randul pentru un numar initial de iteratii
+        for (int i = 0; i < 30; i++)
+        {
+            if (transform.position.y <= -3.5f)
+                transform.position = new Vector2(transform.position.x, 1.75f);
+
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.25f);
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+        randomValue = Random.Range(60, 100);
+
+        // Ajusteaza randomValue pentru a se asigura ca se opreste pe un simbol valid
+        switch (randomValue % 3)
+        {
+            case 1:
+                randomValue += 2;
+                break;
+            case 2:
+                randomValue += 1;
+                break;
+        }
+
+        // Continua rotatia randului pentru randomValue iteratii
+        for (int i = 0; i < randomValue; i++)
+        {
+            if (transform.position.y <= -3.5f)
+                transform.position = new Vector2(transform.position.x, 1.75f);
+
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.25f);
+
+            // Mareste treptat intervalul de timp pentru a incetini rotatia
+            if (i > Mathf.RoundToInt(randomValue * 0.25f))
+                timeInterval = 0.05f;
+            if (i > Mathf.RoundToInt(randomValue * 0.5f))
+                timeInterval = 0.1f;
+            if (i > Mathf.RoundToInt(randomValue * 0.75f))
+                timeInterval = 0.15f;
+            if (i > Mathf.RoundToInt(randomValue * 0.95f))
+                timeInterval = 0.2f;
+
+            yield return new WaitForSeconds(timeInterval);
+        }
+
+        // Stabileste simbolul la care s-a oprit randul in functie de pozitia finala
+        if (transform.position.y == -3.5f)
+            stoppedSlot = "Diamond";
+        else if (transform.position.y == -2.75f)
+            stoppedSlot = "Crown";
+        else if (transform.position.y == -2f)
+            stoppedSlot = "Melon";
+        else if (transform.position.y == -1.25f)
+            stoppedSlot = "Bar";
+        else if (transform.position.y == -0.5f)
+            stoppedSlot = "Seven";
+        else if (transform.position.y == 0.25f)
+            stoppedSlot = "Lemon";
+        else if (transform.position.y == 1.75f)
+            stoppedSlot = "Diamond";
+
+        rowStopped = true; 
+    }
+
+    private void OnDestroy()
+    {
+        GameControl.HandlePulled -= StartRotating; 
+    }
+}
